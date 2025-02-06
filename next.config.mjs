abefd9bf3131a61b2537dc98f775e1b9a6ebbddc
benedictbeejay <benedictbeejay@gmail.com>
2025-02-06 12:40:38 +0100

    completed footer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // images: {
+//   //   remotePatterns: [
+//   //     {
+//   //       protocol: "https",
+//   //       hostname: "www.pexels.com",
+//   //     },
+//   //   ],
+//   // },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
